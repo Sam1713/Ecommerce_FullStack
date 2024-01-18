2cc21f9f6569f388c8336efb164068f534d73374
Sam1713 <samsaju399@gmail.com>
2024-01-19 01:52:37 +0530

    changes are done in mongodb atlas

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
+    await mongoose.connect(process.env.MONGODB_ATLAS_URL ,{
      
     });
     console.log('Connected to MongoDB');
